9c3214802d486bfb192973530fcaaf607ae66b2e
hearsid <hearsid@github.om>
2018-03-04 22:03:32 +0530

    ts changes to support server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,46 @@
-import 'reflect-metadata';
+
+// These are important and needed before anything else
 import 'zone.js/dist/zone-node';
-import { platformServer, renderModuleFactory } from '@angular/platform-server'
-import { enableProdMode } from '@angular/core'
-import * as express from 'express';
-import { readFileSync } from 'fs';
-import { join } from 'path';
+import 'reflect-metadata';
 import { CONTACTS as contacts} from './app/contact/mock-contacts';
 
-const PORT = 4000;
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
+import { enableProdMode } from '@angular/core';
 
+import * as express from 'express';
+import { join } from 'path';
+
+// Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
+// Express server
 const app = express();
 
-const template = readFileSync(join(__dirname, '..', 'dist', 'index.html')).toString();
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist');
 
-app.engine('html', (_, options, callback) => {
-  const opts = { document: template, url: options.req.url };
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
 
-  renderModuleFactory(AppServerModuleNgFactory, opts)
-    .then(html => callback(null, html));
-});
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+
+app.engine('html', ngExpressEngine({
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
+}));
 
 app.set('view engine', 'html');
-app.set('views', 'src')
+app.set('views', join(DIST_FOLDER, 'browser'));
 
-app.get('*.*', express.static(join(__dirname, '..', 'dist')));
+// TODO: implement data requests securely
+app.get('/api/*', (req, res) => {
+  res.status(404).send('data requests are not supported');
+});
+
 
 app.get('/getContacts', (req, res) => {
   const numberOfContacts = req.query.no_of_contacts || 10; // by default 10 contacts
@@ -46,17 +60,15 @@ app.get('/getContacts', (req, res) => {
 
 });
 
-app.get('/test', (req, res) => {
-  return res.json({ test: {first: 'this is the test string'} });
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
 
-});
-
+// All regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render('index', { req });
+  res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
 });
 
-
-
+// Start up the Node server
 app.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}!`);
+  console.log(`Node server listening on http://localhost:${PORT}`);
 });
